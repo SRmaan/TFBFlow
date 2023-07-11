8532042a17ca0280607222320c574fe613becb54
Steve Maan <maansteve@gmail.com>
2023-07-11 02:23:39 -0400

    fixed errors during production build

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
 
+
 export const metadata: Metadata = {
   title: {
     default: siteConfig.name,
@@ -33,7 +34,6 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <>
       <html lang="en" suppressHydrationWarning>
-        <head />
         <body
           className={cn(
             "min-h-screen bg-background font-sans antialiased",
--- components/flow.tsx
+++ components/flow.tsx
@@ -17,45 +17,51 @@ const flow = () => {
         <AccordionTrigger>Home Screen</AccordionTrigger>
         <AccordionContent>
         Implement User Registration and Authentication: Build the registration and login functionality to allow barbers to create accounts and authenticate themselves. Utilize secure
-        authentication mechanisms, such as email/password or social login (e.g., using OAuth).
+        authentication mechanisms, such as email/password or social login e.g., using OAuth.
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-2">
         <AccordionTrigger>Barber Profile</AccordionTrigger>
         <AccordionContent>
-        Design and implement the barber's profile section where they can provide personal information, barber shop details,
+        Design and implement the barber&apos;s profile section where they can provide personal information, barber shop details,
         services offered, availability, pricing, and other relevant information.
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-3">
-        <AccordionTrigger>Build Appointment Booking System</AccordionTrigger>
+        <AccordionTrigger>Customer Profile</AccordionTrigger>
         <AccordionContent>
-        Build Appointment Booking System: Develop features that allow customers to view barbers' availability, select services, and
-        book appointments. Implement a calendar or scheduling system to manage appointments and handle conflicts.
+        Customer Profile
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-4">
+        <AccordionTrigger>Build Appointment Booking System</AccordionTrigger>
+        <AccordionContent>
+        Build Appointment Booking System: Develop features that allow customers to view barbers availability, select services, and
+        book appointments. Implement a calendar or scheduling system to manage appointments and handle conflicts.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-5">
         <AccordionTrigger>Integrate Payment Gateway</AccordionTrigger>
         <AccordionContent>
         Integrate a secure payment gateway to handle transactions between barbers and customers. Implement features for accepting
         payments, issuing invoices, and managing financial transactions.
         </AccordionContent>
       </AccordionItem>
-      <AccordionItem value="item-5">
+      <AccordionItem value="item-6">
         <AccordionTrigger>Implement Reviews and Ratings</AccordionTrigger>
         <AccordionContent>
-        Develop functionality for customers to leave reviews and ratings for barbers' services. Display overall ratings and reviews on
-        the barber's profile to help customers make informed decisions.
+        Develop functionality for customers to leave reviews and ratings for barbers services. Display overall ratings and reviews on
+        the barber&apos;s profile to help customers make informed decisions.
         </AccordionContent>
       </AccordionItem>
-      <AccordionItem value="item-6">
+      <AccordionItem value="item-7">
         <AccordionTrigger>Handle Notifications</AccordionTrigger>
         <AccordionContent>
         Implement push notifications to send appointment reminders, updates, or other relevant notifications to both barbers and customers. Utilize platform-specific
         notification services or third-party libraries for this functionality.
         </AccordionContent>
       </AccordionItem>
-      <AccordionItem value="item-7">
+      <AccordionItem value="item-8">
         <AccordionTrigger>Ensure Security and Data Privacy</AccordionTrigger>
         <AccordionContent>
         Implement appropriate security measures to protect user data and sensitive information. Follow best practices for data encryption,
